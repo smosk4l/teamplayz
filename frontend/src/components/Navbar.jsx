2c0df6860a379ac581097d706886b697fea79ef7
sm8sk4l <sebastianmoskal@protonmail.com>
2023-05-11 17:57:27 +0200

    navbar component UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,40 @@
+import { useContext, useState } from "react";
 import logo from "../assets/Teamplayz_logo.png";
-import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { HiOutlineMenuAlt4, HiOutlineX } from "react-icons/hi";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
   return (
-    <nav>
+    <nav className="sticky top-0 z-20 overflow-hidden">
       <ul className="flex items-center gap-5 list-none px-4 py-4">
         <li className="mr-auto">
           <img src={logo} alt="Teamplayz logo" />
         </li>
         <li>
-          <button className="bg-[#303030] font-bold text-white px-5 py-2 rounded-lg">
+          <button className="bg-black-button font-bold text-white px-5 py-2 rounded-lg">
             Sign In
           </button>
         </li>
         <li>
-          <HiOutlineMenuAlt4 className="text-2xl" />
+          <button className="text-2xl" onClick={toggleMenu}>
+            {isMenuOpen ? <HiOutlineX /> : <HiOutlineMenuAlt4 />}
+          </button>
         </li>
+      </ul>
+
+      <ul
+        className={`flex items-start flex-col gap-10 ml-12 mt-24 easy-out duration-300 text-link [&>*]:hover:text-black
+
+          ${isMenuOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <li>
+          <a href="#">Features</a>
+        </li>
+        <li>Features</li>
+        <li>Features</li>
+        <li>Features</li>
       </ul>
     </nav>
   );
