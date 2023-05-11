2edb5b5ed8dd5dc3d0179443f7df8dfabbc52c2f
JTarsa <jan.tarsa@protonmail.com>
2023-05-11 19:17:32 +0200

    JWT token

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const {errorHandler} = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-connectDB()
+connectDB();
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use("/api/meetings", require("./routes/meetingsRoutes"));
-app.use(errorHandler)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/meetings", require("./routes/meetingsRoute"));
+app.use("/api/users", require("./routes/userRoute"));
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port:${port}`));
