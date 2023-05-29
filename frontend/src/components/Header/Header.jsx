c60d7b6c82f541602550ca6502ae7b9b78189134
sm8sk4l <sebastianmoskal@protonmail.com>
2023-05-29 18:46:20 +0200

    setting user to userState

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Navbar/Navbar";
 import headerIlustration from "../../assets/header_ilustration.png";
 import Button from "../UI/Button/Button";
+
 function Header() {
   return (
     <header>
---
+++ frontend/src/state/authState.jsx
@@ -0,0 +1,7 @@
+import { create } from "zustand";
+
+const useAuthState = create((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+}));
+export default useAuthState;
