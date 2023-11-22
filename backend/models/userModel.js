75018f02c937c7448eb000a9415476b3cc391599
JTarsa <jan.tarsa@protonmail.com>
2023-11-22 01:43:08 +0100

    authorizeUserViaEmail

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,26 +1,36 @@
 const mongoose = require('mongoose')
 const userSchema = mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: [true, 'Please add a first name'],
+    {
+        firstName: {
+            type: String,
+            required: [true, 'Please add a first name'],
+        },
+        lastName: {
+            type: String,
+            required: [true, 'Please add a last name'],
+        },
+        photo:{
+            type : String
+        },
+        email: {
+            type: String,
+            required: [true, 'Please add an email'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Please add a password'],
+        },
+        activationCode: {
+            type: String,
+        },
+        isAuthorized: {
+            type: Boolean,
+            default : false,
+        },
     },
-    lastName: {
-      type: String,
-      required: [true, 'Please add a last name'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Please add an email'],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Please add a password'],
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 )
 module.exports = mongoose.model('User', userSchema)
