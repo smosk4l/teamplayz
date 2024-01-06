3b318cf047db78386afbfefa98f85e7ea4a6a382
JTarsa <jan.tarsa@protonmail.com>
2024-01-06 23:50:56 +0100

    forgetPassword

--- backend/middleware/sendEmail.js
+++ backend/middleware/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
-async function sendMail(email, activationCode) {
+async function sendMail(email, activationCode, type) {
   const transporter = nodemailer.createTransport({
     host: process.env.HOST,
     port: 587,
@@ -12,11 +12,24 @@ async function sendMail(email, activationCode) {
       pass: process.env.PASSWORD,
     },
   });
+
+  let subject, text;
+
+  if (type === 'activation') {
+    subject = 'Potwierdzenie konta';
+    text = `Aby potwierdzić swoje konto, kliknij w link poniżej: http://localhost:8000/api/users/activate/${activationCode}`;
+  } else if (type === 'passwordReset') {
+    subject = 'Resetowanie hasła';
+    text = `Aby zresetować hasło, kliknij w link poniżej: http://localhost:8000/api/users/reset-password/${activationCode}`;
+  } else {
+    throw new Error('Nieprawidłowy typ wiadomości');
+  }
+
   await transporter.sendMail({
     from: '"Teamplayz" <f:teamplayzverify@gmail.com>',
     to: `${email}`,
-    subject: 'CHUJ CI W DUPE!',
-    text: `Aby potwierdzić swoje konto, kliknij w link poniżej: http://localhost:8000/api/users/activate/${activationCode}`,
+    subject: subject,
+    text: text,
   });
 }
 
